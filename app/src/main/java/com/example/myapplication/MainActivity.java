a3a9f8b176e7028e8405255bef5cc5cd1f0b3139
ClarNuOMandarina <131393141+ClarNuOMandarina@users.noreply.github.com>
2025-04-04 12:07:12 +0300

    Backup Radu

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,4 +1,9 @@
 package com.example.myapplication;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MainActivity {
+
 }
--- app/src/main/java/com/example/myapplication/Task.java
+++ app/src/main/java/com/example/myapplication/Task.java
@@ -1,26 +1,26 @@
-package com.example.myapplication;
-
-public class Task {
-    public String Title;
-    public boolean IsCompleted;
-
-    public Task(String Title){
-        this.Title=Title;
-        IsCompleted=false;
-    }
-
-    public String getTitle(){
-        return Title;
-    }
-
-    public void setTitle(String Title){
-        this.Title=Title;
-    }
-    public boolean IsCompleted(){
-        return IsCompleted;
-    }
-
-    public void setCompleted(boolean Completed){
-        IsCompleted=Completed;
-    }
-}
+//package com.example.myapplication;
+//
+//public class Task {
+//    public String Title;
+//    public boolean IsCompleted;
+//
+//    public Task(String Title){
+//        this.Title=Title;
+//        IsCompleted=false;
+//    }
+//
+//    public String getTitle(){
+//        return Title;
+//    }
+//
+//    public void setTitle(String Title){
+//        this.Title=Title;
+//    }
+//    public boolean IsCompleted(){
+//        return IsCompleted;
+//    }
+//
+//    public void setCompleted(boolean Completed){
+//        IsCompleted=Completed;
+//    }
+//}
